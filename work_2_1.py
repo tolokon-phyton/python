be943162c5f888b76856eb5e403e387ee792d4c3
Tolokon <tolokon@list.ru>
2020-09-30 22:17:03 +0300

    Первая версия

---
+++ work_2_1.py
@@ -0,0 +1,12 @@
+shit1 = [8, 2.46, 'dot', None, 18, 'Hello!', True, 244]
+a: int = len (shit1)
+print('Длина списка составляет: ', len (shit1))
+n: int = 0
+
+while (n < a):
+    print(type(shit1[n]))
+    n = n + 1
+
+
+
+
